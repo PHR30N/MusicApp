a9917614e861885a2c0104a001988e363e240bd4
PHR30N <khtabmhmdahmd11@gmail.com>
2025-05-09 18:50:16 +0300

    test 1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace MusicApp
     {
         static void Main(string[] args)
         {
+            // Create a new instance of the MusicPlayer class
+            MusicPlayer player = new MusicPlayer();
+            // Add some songs to the playlist
+            player.AddSong("Song 1");
+            player.AddSong("Song 2");
+            player.AddSong("Song 3");
+            // Play the first song
+            player.Play(0);
+            // Pause the song
+            player.Pause();
+            // Resume playing the song
+            player.Resume();
+            // Stop playing the song
+            player.Stop();
+            // Remove a song from the playlist
+            player.RemoveSong(1);
+            // Display the remaining songs in the playlist
+            player.DisplayPlaylist();
         }
     }
 }
